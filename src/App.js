9967c3191199969f4060e4696bd754deb22530d5
Michael Irby <michael.irbstoomuch@gmail.com>
2017-01-14 12:31:13 -0500

    MapController component added as well as navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
 import React, { Component } from 'react';
 import './css/App.css';
+import MapController from './MapController.js';
 
 class App extends Component {
     render() {
         return (
-            <div>
-                Welcome to our Geography Game
+            <div className="app-wrapper">
+                <nav className="navbar navbar-default" id="navbar">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <h1>Geography Game</h1>
+                        </div>
+                    </div>
+                </nav>
+                <div className="container">
+                    <div className="row">
+                        <MapController />
+                    </div>
+                </div>
+                <div className="container">
+                    <div className="row">
+                    </div>
+                </div>
             </div>
         );
     }
---
+++ src/MapController.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class MapController extends Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			mapOptions: {
+				center: {lat: 39.8282, lng: -98.5795},
+				zoom: 4
+			}
+		}
+	}
+	render(){
+		return(
+			<div>Map will go here</div>
+		)
+	}
+}
+
+export default MapController;
