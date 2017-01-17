40ef8720fd141aa9f7a9bfed724b424076f879be
Drew Parker <drew.i.parker@gmail.com>
2017-01-17 12:00:02 -0500

    Added import Bootstrap import line

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './css/App.css';
 import MapController from './MapController.js';
 
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 class App extends Component {
     render() {
         return (
