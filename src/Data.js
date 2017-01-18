3e8218910fc3f55645dd2a21bd26f20ac43ac274
Michael Irby <michael.irbstoomuch@gmail.com>
2017-01-18 18:26:48 -0500

    Built out the Data.js file full of objects

--- src/Data.js
+++ src/Data.js
@@ -2,9 +2,9 @@ module.exports = [
     {
         question: "What is the westernmost point to the lower 48 states?",
         answer: "Cape Alava, WA",
-        center: {lat: 47.2823941, lng: -124.1748917},
+        center: {lat: 39.8282, lng: -98.5795},
         position: {lat: 48.1647572, lng: -124.7331501},
-        questionZoom: 6,
+        questionZoom: 4,
         answerZoom: 14,
         // styles: mapStyle
     },
@@ -12,7 +12,79 @@ module.exports = [
         question: "This city on America’s largest river shares a name with the capital of Egypt.",
         answer: "Cairo, IL",
         center: {lat: 39.8282, lng: -98.5795},
-        position: {lat: 37.0295624, lng: -89.2529051},
+        position: {lat: 37.0058053, lng: -89.1777963},
+        questionZoom: 4,
+        answerZoom: 14,
+        // styles: mapStyle
+    },
+    {
+        question: "This island is a volcanic cone in America’s deepest Lake. Wizard Island.",
+        answer: "Crater Lake, OR",
+        center: {lat: 39.8282, lng: -98.5795},
+        position: {lat: 42.938841, lng: -122.145989},
+        questionZoom: 4,
+        answerZoom: 14,
+        // styles: mapStyle
+    },
+    {
+        question: "Where is the site of largest active alpine glacier in the lower 48 states?",
+        answer: "Mt Rainer, WA",
+        center: {lat: 39.8282, lng: -98.5795},
+        position: {lat: 46.852148, lng: -121.757732},
+        questionZoom: 4,
+        answerZoom: 14,
+        // styles: mapStyle
+    },
+    {
+        question: "This is the tallest mountain east of the Mississippi River.",
+        answer: "Mt Mitchell, NC",
+        center: {lat: 39.8282, lng: -98.5795},
+        position: {lat: 35.764963, lng: -82.265140},
+        questionZoom: 4,
+        answerZoom: 14,
+        // styles: mapStyle
+    },
+    {
+        question: "This is the location of America’s largest caldera.",
+        answer: "Yellowstone, WY",
+        center: {lat: 39.8282, lng: -98.5795},
+        position: {lat: 44.427801, lng: -110.588431},
+        questionZoom: 4,
+        answerZoom: 14,
+        // styles: mapStyle
+    },
+    {
+        question: "This site in America is known as “the Four Corners”.",
+        answer: "Border of CO, AZ, UT, and NM",
+        center: {lat: 39.8282, lng: -98.5795},
+        position: {lat: 36.999082, lng: -109.045224},
+        questionZoom: 4,
+        answerZoom: 14,
+        // styles: mapStyle
+    },
+    {
+        question: "This largest city in FL lies on one of the few rivers that flows from South to North.",
+        answer: "Jacksonville, FL",
+        center: {lat: 39.8282, lng: -98.5795},
+        position: {lat: 30.324650, lng: -81.659913},
+        questionZoom: 4,
+        answerZoom: 14,
+        // styles: mapStyle
+    },
+    {
+        question: "What is the largest city on the shore of the smallest Great Lake?",
+        answer: "Toronto, ON",
+        center: {lat: 39.8282, lng: -98.5795},
+        position: {lat: 43.645147, lng: -79.380863},
+        questionZoom: 4,
+        answerZoom: 14,
+        // styles: mapStyle
+    },
+    {
+        question: "The city got its current name from railroad engineer J. Edgar Thompson. It’s thought to be a shortened version of “Atlantica-Pacifica.”",
+        answer: "Atlanta, GA",
+        center: {lat: 39.8282, lng: -98.5795},
+        position: {lat: 33.748777, lng: -84.388246},
         questionZoom: 4,
         answerZoom: 14,
         // styles: mapStyle
