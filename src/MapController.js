09e5831d3c719fd5862e81f3b98725cb8943032d
Drew Parker <drew.i.parker@gmail.com>
2017-01-19 09:09:41 -0500

    Commented in MapController

--- src/MapController.js
+++ src/MapController.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 import { withGoogleMap, GoogleMap, Marker } from 'react-google-maps'
 
 
-// RIGHT NOW THIS IS NOT A CLASS!! IT'S A (PURE) RENDER FUNCTION
+// The Google Maps Higher order component. MapController is a function that
+// returns the GoogleMap component. The GoogleMap component is responsible
+// for providing a Map with unique properties. A callback is used to load the
+// map.
 const MapController = withGoogleMap(props => (
-  <GoogleMap
-    ref={props.onMapLoad}
-    defaultZoom={props.data.questionZoom}
-    center={props.data.center}
-    onClick={props.onMapClick}
-  >
-    <Marker position={props.data.position} visible={props.data.visible} />
-  </GoogleMap>
+    <GoogleMap
+        ref={props.onMapLoad}
+        defaultZoom={props.data.questionZoom}
+        center={props.data.center}
+        onClick={props.onMapClick}>
+        <Marker
+            position={props.data.position}
+            visible={props.data.visible}>
+        </Marker>
+    </GoogleMap>
 ));
 
 export default MapController;
