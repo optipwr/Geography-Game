d6c88e072fadc8a37dfc3d9e86809bb13cb5d946
Kaplaugher <kaplaugher94@gmail.com>
2017-01-20 09:11:39 -0500

    fixed errors

--- src/Navbar.js
+++ src/Navbar.js
@@ -5,9 +5,9 @@ class Navbar extends Component {
         return(
             <nav className="navbar navbar-default" id="navbar">
                 <div className="container-fluid">
-                  <button type="button" className="btn btn-warning navbar-btn"><span class="glyphicon glyphicon-check"></span> Instructions</button>
-                  <button type="button" className="btn btn-danger navbar-btn"><span class="glyphicon glyphicon-remove"></span> About</button>
-                  <button type="button" className="btn btn-success navbar-btn"><span class="glyphicon glyphicon-ok"></span> ASIAN-MODE</button>
+                  <button type="button" className="btn btn-warning navbar-btn"><span className="glyphicon glyphicon-flag"></span> Instructions</button>
+                  <button type="button" className="btn btn-danger navbar-btn"><span className="glyphicon glyphicon-user"></span> About</button>
+                  <button type="button" className="btn btn-success navbar-btn"><span className="glyphicon glyphicon-plane"></span> ASIAN-MODE</button>
                     <div className="navbar-header">
                         <h1 className="title">Geography Game</h1>
 
