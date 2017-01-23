f68a846e73f65a77f8f2633c129f598643cfeb3e
Sean Bhupathi <seanbhupathi@gmail.com>
2017-01-23 10:34:42 -0500

    navbar update just need to add router and some styling

--- src/Navbar.js
+++ src/Navbar.js
@@ -9,30 +9,23 @@ class Navbar1 extends Component {
     render() {
         return (
             <Navbar inverse collapseOnSelect>
-    <Navbar.Header>
-      <Navbar.Brand>
-        <a href="#">React-Bootstrap</a>
-      </Navbar.Brand>
-      <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <NavItem eventKey={1} href="#">Link</NavItem>
-        <NavItem eventKey={2} href="#">Link</NavItem>
-        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-          <MenuItem eventKey={3.1}>Action</MenuItem>
-          <MenuItem eventKey={3.2}>Another action</MenuItem>
-          <MenuItem eventKey={3.3}>Something else here</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={3.3}>Separated link</MenuItem>
-        </NavDropdown>
-      </Nav>
-      <Nav pullRight>
-        <NavItem eventKey={1} href="#">Link Right</NavItem>
-        <NavItem eventKey={2} href="#">Link Right</NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+                <Navbar.Header>
+                    <Navbar.Brand>
+                        <a href="#"><img src="https://www.globalbrigades.org/media/Globe.png" className="logo"/></a>
+                        <a href="#"> Geography Game</a>
+                    </Navbar.Brand>
+                    <Navbar.Toggle />
+                </Navbar.Header>
+                <Navbar.Collapse>
+                <Nav>
+                    <NavItem eventKey={1} href="#">Play</NavItem>
+                    <NavItem eventKey={2} href="#">Instructions</NavItem>
+                </Nav>
+                <Nav pullRight>
+                    <NavItem eventKey={1} href="#"></NavItem>
+                </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         )
     }
 }
